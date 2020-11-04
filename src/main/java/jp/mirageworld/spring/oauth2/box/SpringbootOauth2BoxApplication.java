fca9b1e3ab1397f1b51e140995639f2b5f629f6b
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-11-04 15:15:41 +0900

    initialize

---
+++ src/main/java/jp/mirageworld/spring/oauth2/box/SpringbootOauth2BoxApplication.java
@@ -0,0 +1,13 @@
+package jp.mirageworld.spring.oauth2.box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOauth2BoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOauth2BoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/mirageworld/spring/oauth2/box/SpringbootOauth2BoxApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.mirageworld.spring.oauth2.box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOauth2BoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
